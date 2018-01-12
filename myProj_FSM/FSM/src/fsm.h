a7d72f684b57e442d17e09521f41a0a7a90b9e96
kevin-ledinh <kevin-ledinh@users.noreply.github.com>
2018-01-12 09:28:09 +0000

    committing skeleton files for fsm dev.

---
+++ myProj_FSM/FSM/src/fsm.h
@@ -0,0 +1,5 @@
+#ifndef _FSM_H
+#define _FSM_H
+
+
+#endif // _FSM_H
---
+++ myProj_FSM/FSM/test/test_fsm.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "fsm.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_fsm_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement fsm");
+}
